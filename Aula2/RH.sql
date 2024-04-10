7a6a1cfa6396b2123f5e4d35fddfb27fc938bf69
Andre <andrenas.nascimento95@gmail.com>
2024-04-10 16:00:08 -0300

    Projeto Hello World

---
+++ Aula2/RH.sql
@@ -0,0 +1,44 @@
+create database RH;
+
+use RH;
+create table tb_funcionarios(
+id bigint auto_increment, primary key
+nome varchar(255) not null,
+data_admissao date,
+função varchar(255) not full,
+matricula varchar(255) not full
+salario: decimal 
+preco decimal not null,
+primary key(id),
+);
+
+
+select * from tb_produtos;
+
+
+
+
+
+select * from tb_funcionarios;
+select * from tb_funcionarios where > 2000.00;
+select * from tb_funcionarios where < 2000.00;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
